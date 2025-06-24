62e590a4c53426ce6180c1adcbcfa714175d5de9
Sibo Wang-Chen <sibo.wang@epfl.ch>
2025-06-24 23:39:46 +0200

    Update config.py

--- noise_monitor/config.py
+++ noise_monitor/config.py
@@ -10,4 +10,4 @@ clip_length = 300  # seconds, must be a factor of 3600 (1h)
 channels = 2  # stereo
 chunk_size = 4096  # number of frames per
 format = "int32"  # bits per sample
-recording_hours = [17, 23, 0, 1, 2, 3, 4, 5, 6, 7]  # hours of the day to record
+recording_hours = [23, 0, 1, 2, 3, 4, 5, 6, 7]  # hours of the day to record
